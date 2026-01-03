24e22a9f2c134a60fcb10435c284d7b069620565
Aarsh <aarsh8210@gmail.com>
2026-01-03 10:44:27 +0530

    Connect login page with backend authentication

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,30 @@
 function login() {
-    alert("Login functionality will be connected to backend");
-}
+    const email = document.querySelector('input[type="email"]').value;
+    const password = document.querySelector('input[type="password"]').value;
+    const role = document.querySelector('select').value;
 
-function checkIn() {
-    alert("Checked in");
-}
-
-function checkOut() {
-    alert("Checked out");
+    fetch("http://127.0.0.1:5000/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password
+        })
+    })
+    .then(res => {
+        if (!res.ok) throw new Error("Invalid login");
+        return res.json();
+    })
+    .then(data => {
+        if (data.role === "employee") {
+            window.location.href = "employee_dashboard.html";
+        } else if (data.role === "admin") {
+            window.location.href = "admin_dashboard.html";
+        }
+    })
+    .catch(err => {
+        alert("Login failed");
+    });
 }
