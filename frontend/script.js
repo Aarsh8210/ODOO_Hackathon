b019476aab22c3d0dec96498345bb91a553eab39
Shubham192007 <t6nkq6cvkm@privaterelay.appleid.com>
2026-01-03 10:13:43 +0530

    Implement Dayflow HRMS frontend pages

---
+++ frontend/script.js
@@ -0,0 +1,11 @@
+function login() {
+    alert("Login functionality will be connected to backend");
+}
+
+function checkIn() {
+    alert("Checked in");
+}
+
+function checkOut() {
+    alert("Checked out");
+}
